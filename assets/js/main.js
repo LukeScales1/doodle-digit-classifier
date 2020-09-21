6b0c27493524beb5366267e139f45d038b3c4374
LukeScales1 <luke.scales2@gmail.com>
2020-09-21 12:40:34 -0700

    Added remaining event listeners, drawing to canvas successfully; Set width and height of canvas in atributes to fix janky coordinate finding

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let drawing = false;
-const sketch = {
+const doodle = {
 	x: [],
 	y: []
 }
@@ -7,28 +7,46 @@ const sketch = {
 window.onload = () => {
 	const canvas = document.getElementById('doodle-pad');
 	const ctx = canvas.getContext('2d');
-	ctx.fillStyle = '#fff';
+	ctx.strokeStyle = '#ffffff';
 	ctx.lineJoin = 'round';
 	ctx.lineWidth = '12';
 
 	canvas.addEventListener('mousedown', (e) => {
 		drawing = true;
-		console.log("it's touching me!");
-		const xval = e.pageX - canvas.offsetLeft;
-		const yval = e.pageY - canvas.offsetTop;
-
-		console.log(`${xval} ${yval}`);
-		sketch.x.push(xval);
-		sketch.y.push(yval);
-
+		
+		addCoordinates(e);
 	});
 
+	canvas.addEventListener('mousemove', (e) => {
+		if (drawing === true) {
+			addCoordinates(e);
+			drawDoodle();
+		}
+	});
+
+	canvas.addEventListener('mouseup', (e) => {
+		if (drawing === true) {
+			addCoordinates(e);
+			drawDoodle();
+			drawing = false;
+		}
+	});
+
+	addCoordinates = (e) => {
+		const xval = e.clientX - canvas.offsetLeft;
+		const yval = e.clientY - canvas.offsetTop;
+
+		doodle.x.push(xval);
+		doodle.y.push(yval);
+	}
+
 	drawDoodle = () => {
-		for (let i = 0; i < sketch.x.length-1; i++) {
-			if (sketch.x[i+1]) {
+		for (let i = 0; i < doodle.x.length-1; i++) {
+			if (doodle.x[i+1]) {
+				console.log('drawing');
 				ctx.beginPath();
-				ctx.moveTo(sketch.x[i], sketch.y[i]);
-				ctx.lineTo(xval, yval);
+				ctx.moveTo(doodle.x[i], doodle.y[i]);
+				ctx.lineTo(doodle.x[i+1], doodle.y[i+1]);
 				ctx.closePath();
 				ctx.stroke();
 			}
